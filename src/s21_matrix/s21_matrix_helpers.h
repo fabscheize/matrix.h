47374b372702930c1715dc76cdc08f54ce2696c8
Vlad Karsten <vkarsten1111@icloud.com>
2024-04-15 10:39:34 +0700

    Added: project dirs & Makefile

---
+++ src/s21_matrix/s21_matrix_helpers.h
@@ -0,0 +1,14 @@
+#ifndef S21_MATRIX_HELPERS_H_
+#define S21_MATRIX_HELPERS_H_
+
+#include <math.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "../s21_matrix.h"
+
+enum retutn_values { k_ok, k_invalid_matrix, k_calc_err };
+enum comparison { k_not_equal, k_equal };
+
+#endif  // S21_MATRIX_HELPERS_H_
